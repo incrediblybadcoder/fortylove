d2278883792097f0fb7030b0d71eb15446040267
Yoohnas <jonas.cahenzli@gmail.com>
2023-04-19 22:23:31 +0200

    SQL File to create first tabel and pre-fill it [skip ci]

---
+++ src/main/resources/schema/createTables.sql
@@ -0,0 +1,8 @@
+IF OBJECT_ID('tennis_clubs', 'U') IS NOT NULL
+DROP TABLE tennis_clubs;
+
+CREATE TABLE tennis_clubs (
+                              tennis_club_id INT PRIMARY KEY IDENTITY(1,1),
+                              name VARCHAR(50) NOT NULL,
+                              adresse VARCHAR(100) NOT NULL
+);
---
+++ src/main/resources/schema/fillTables.sql
@@ -0,0 +1,14 @@
+INSERT INTO tennis_clubs (name, adresse)
+VALUES
+    ('TC Grün-Weiss', 'Musterstraße 1, 12345 Musterstadt'),
+    ('Tennis-Club 2000', 'Hauptstraße 10, 54321 Hauptstadt'),
+    ('TC Schmetterball', 'Blumenweg 7, 67890 Blumenstadt'),
+    ('Tennisclub am Park', 'Parkstraße 22, 98765 Parkstadt'),
+    ('TC Aufschlag', 'Sportplatz 5, 23456 Sportstadt'),
+    ('Tennisclub am See', 'Seestraße 11, 34567 Seestadt'),
+    ('TC Netzroller', 'Marktplatz 3, 45678 Marktstadt'),
+    ('Tennisclub am Berg', 'Bergstraße 8, 56789 Bergstadt'),
+    ('TC Vorhand', 'Spielstraße 4, 67890 Spielstadt'),
+    ('TC Rückhand', 'Gaidla 3, 60006 Spielstadt'),
+    ('TC Doppelfehler', 'Immer und Überall 7, 65489 Doppelstadt'),
+    ('Tennisclub am Fluss', 'Flussweg 17, 78901 Flussstadt');
