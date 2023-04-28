18218cc1f6c136678ef56f97c81076db7800eb82
Yoohnas <jonas.cahenzli@gmail.com>
2023-04-28 12:23:24 +0200

    #44 Persons replaced by Users [skip ci]

--- src/main/resources/schema/fillTables.sql
+++ src/main/resources/schema/fillTables.sql
@@ -1,14 +1,14 @@
 INSERT INTO tennis_clubs (name, address)
-VALUES
-    ('TC Grün-Weiss', 'Musterstraße 1, 12345 Musterstadt'),
-    ('Tennis-Club 2000', 'Hauptstraße 10, 54321 Hauptstadt'),
-    ('TC Schmetterball', 'Blumenweg 7, 67890 Blumenstadt'),
-    ('Tennisclub am Park', 'Parkstraße 22, 98765 Parkstadt'),
-    ('TC Aufschlag', 'Sportplatz 5, 23456 Sportstadt'),
-    ('Tennisclub am See', 'Seestraße 11, 34567 Seestadt'),
-    ('TC Netzroller', 'Marktplatz 3, 45678 Marktstadt'),
-    ('Tennisclub am Berg', 'Bergstraße 8, 56789 Bergstadt'),
-    ('TC Vorhand', 'Spielstraße 4, 67890 Spielstadt'),
-    ('TC Rückhand', 'Gaidla 3, 60006 Spielstadt'),
-    ('TC Doppelfehler', 'Immer und Überall 7, 65489 Doppelstadt'),
-    ('Tennisclub am Fluss', 'Flussweg 17, 78901 Flussstadt');
+VALUES ('Wimbledon', 'Church Road, London SW19 5AE'),
+       ('Roland Garros', '2 Avenue Gordon Bennett, 75016 Paris, France'),
+       ('US Open', 'Flushing Meadows-Corona Park, Queens, NY 11368, USA'),
+       ('Australian Open', 'Olympic Blvd, Melbourne VIC 3001, Australia'),
+       ('Indian Wells', '78200 Miles Ave, Indian Wells, CA 92210, USA');
+
+INSERT INTO users (name)
+VALUES ('Alice'),
+       ('Bob'),
+       ('Charlie'),
+       ('Dave'),
+       ('Emma');
+
