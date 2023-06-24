11d61bbfbe4c61cc4f0e539a82dd379414b04e39
Yoohnas <jonas.cahenzli@gmail.com>
2023-06-24 16:39:02 +0200

    #87 Unit Test for TestSessionServiceImpl created

--- src/test/java/ch/fortylove/persistence/service/TestSessionServiceImpl.java
+++ src/test/java/ch/fortylove/persistence/service/TestSessionServiceImpl.java
@@ -2,23 +2,61 @@ package ch.fortylove.persistence.service;
 
 import ch.fortylove.SpringTest;
 import ch.fortylove.persistence.entity.User;
-import org.junit.jupiter.api.Assertions;
+import ch.fortylove.security.SecurityService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
-@SpringTest
-class TestSessionServiceImpl {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-    @Autowired SessionService sessionServiceTestee;
+@SpringTest
+public class TestSessionServiceImpl {
+
+    private SessionServiceImpl sessionService;
+    private SecurityService securityService;
+    private UserService userService;
+
+    @BeforeEach
+    public void setUp() {
+        securityService = mock(SecurityService.class);
+        userService = mock(UserService.class);
+        sessionService = new SessionServiceImpl(userService, securityService);
+    }
 
     @Test
-    void testGetCurrentUser() {
+    public void testGetCurrentUser() {
         // Arrange
+        UserDetails userDetails = mock(UserDetails.class);
+        when(userDetails.getUsername()).thenReturn("testuser");
+        when(securityService.getAuthenticatedUser()).thenReturn(Optional.of(userDetails));
+        User expectedUser = new User();
+        expectedUser.setFirstName("firstname");
+        expectedUser.setLastName("lastname");
+        when(userService.findByEmail("testuser")).thenReturn(Optional.of(expectedUser));
+
         // Act
-        final Optional<User> currentUser = sessionServiceTestee.getCurrentUser();
+        Optional<User> result = sessionService.getCurrentUser();
+
         // Assert
-        Assertions.assertEquals(true, true);
+        assertTrue(result.isPresent());
+        assertEquals(expectedUser, result.get());
+    }
+
+    @Test
+    public void testGetCurrentUserWhenNoAuthenticatedUser() {
+        // Arrange
+        when(securityService.getAuthenticatedUser()).thenReturn(Optional.empty());
+
+        // Act
+        Optional<User> result = sessionService.getCurrentUser();
+
+        // Assert
+        assertFalse(result.isPresent());
     }
 }
