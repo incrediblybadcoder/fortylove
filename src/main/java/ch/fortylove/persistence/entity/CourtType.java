83051c8e6e8288ff44815156709fbb020d6cc851
incrediblybadcoder <m.solombrino@hotmail.ch>
2023-07-23 14:35:14 +0200

    #154 App Icon auf TCU ändern - changed app icon - added court field for courtIcon to seperate court type from court icon - added flyway script to add courtIcon column [skip-ci]

--- src/main/java/ch/fortylove/persistence/entity/CourtType.java
+++ src/main/java/ch/fortylove/persistence/entity/CourtType.java
@@ -3,21 +3,18 @@ package ch.fortylove.persistence.entity;
 import javax.annotation.Nonnull;
 
 public enum CourtType {
-    CLAY("C", "Sand", "icons/icon-court-clay.svg"),
-    GRASS("G", "Grass", "icons/icon-court-grass.svg"),
-    SYNTHETIC("S", "Teppich", "icons/icon-court-synthetic.svg"),
-    HARD("H", "Hart", "icons/icon-court-hard.svg");
+    CLAY("clay", "Sand"),
+    GRASS("grass", "Grass"),
+    SYNTHETIC("synthetic", "Teppich"),
+    HARD("hard", "Hart");
 
     @Nonnull private final String code;
     @Nonnull private final String material;
-    @Nonnull private final String icon;
 
     CourtType(@Nonnull final String code,
-              @Nonnull final String material,
-              @Nonnull final String icon) {
+              @Nonnull final String material) {
         this.code = code;
         this.material = material;
-        this.icon = icon;
     }
 
     @Nonnull
@@ -28,11 +25,6 @@ public enum CourtType {
     @Nonnull
     public String getMaterial() {
         return material;
-    }
-
-    @Nonnull
-    public String getIcon() {
-        return icon;
     }
 
     @Override
