743c89a2ba9d34a2e4f739e34b056159411c2eb9
Yoohnas <jonas.cahenzli@gmail.com>
2023-08-03 18:03:02 +0200

    #177 (rest vom comitt von zuvor) - Develop User als hinzugefügt denn es auch auf der produktiven DB geben soll (damit das Dev-Team mit diesem Login einen ersten Admin User für den Club erstellen kann) - Devlop User Credentials: email: fortylove.unter@gmail.com password -> das Standard PW - Der Develop User soll für den zukünftigen Admin User in der Membemanagment Übersicht nicht ersichtlich sein

--- src/main/java/ch/fortylove/configuration/setupdata/SetupDataLoaderService.java
+++ src/main/java/ch/fortylove/configuration/setupdata/SetupDataLoaderService.java
@@ -1,6 +1,7 @@
 package ch.fortylove.configuration.setupdata;
 
 import ch.fortylove.configuration.setupdata.data.BookingSettingsSetupData;
+import ch.fortylove.configuration.setupdata.data.DefaultUserSetupData;
 import ch.fortylove.configuration.setupdata.data.PlayerStatusSetupData;
 import ch.fortylove.configuration.setupdata.data.PrivilegeSetupData;
 import ch.fortylove.configuration.setupdata.data.RoleSetupData;
@@ -15,16 +16,19 @@ public class SetupDataLoaderService {
     @Nonnull private final PrivilegeSetupData privilegeSetupData;
     @Nonnull private final RoleSetupData roleSetupData;
     @Nonnull private final PlayerStatusSetupData playerStatusSetupData;
+    @Nonnull private final DefaultUserSetupData defaultUserSetupData;
 
     @Autowired
     public SetupDataLoaderService(@Nonnull final BookingSettingsSetupData bookingSettingsSetupData,
                                   @Nonnull final PrivilegeSetupData privilegeSetupData,
                                   @Nonnull final RoleSetupData roleSetupData,
-                                  @Nonnull final PlayerStatusSetupData playerStatusSetupData) {
+                                  @Nonnull final PlayerStatusSetupData playerStatusSetupData,
+                                  @Nonnull final DefaultUserSetupData defaultUserSetupData) {
         this.bookingSettingsSetupData = bookingSettingsSetupData;
         this.privilegeSetupData = privilegeSetupData;
         this.roleSetupData = roleSetupData;
         this.playerStatusSetupData = playerStatusSetupData;
+        this.defaultUserSetupData = defaultUserSetupData;
     }
 
     public void initData() {
@@ -32,5 +36,6 @@ public class SetupDataLoaderService {
         privilegeSetupData.createPrivileges();
         roleSetupData.createRoles();
         playerStatusSetupData.createPlayerStatus();
+        defaultUserSetupData.createDefaultUser();
     }
 }
