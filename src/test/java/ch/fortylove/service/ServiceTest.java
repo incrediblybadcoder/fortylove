1d80f1460aadd0831ca7df508de5e28110aba237
Yoohnas <jonas.cahenzli@gmail.com>
2023-08-15 15:20:20 +0200

    #157 - JavaMailSender für die Test gemocket, damit Spring den UserService injizieren kann.

--- src/test/java/ch/fortylove/service/ServiceTest.java
+++ src/test/java/ch/fortylove/service/ServiceTest.java
@@ -4,9 +4,13 @@ import ch.fortylove.SpringTest;
 import ch.fortylove.testdata.factory.TestDataFactory;
 import jakarta.annotation.Nonnull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringTest
 abstract class ServiceTest {
+
+    @MockBean JavaMailSender javaMailSender;
 
     @Autowired private TestDataFactory testDataFactory;
 
