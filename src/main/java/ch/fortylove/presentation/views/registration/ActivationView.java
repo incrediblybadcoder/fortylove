c1450a4efb54ae839c855002ec3054cef6047e68
Yoohnas <jonas.cahenzli@gmail.com>
2023-08-18 12:04:05 +0200

    #201 - Bevor ein User anhand des Aktivierungslink aktiviert wird, wird neu überprüft ob der User schon aktiviert ist. - Neue Meldung an Enduser bei fehlerhaften / korrupten Aktivierungslinks

--- src/main/java/ch/fortylove/presentation/views/registration/ActivationView.java
+++ src/main/java/ch/fortylove/presentation/views/registration/ActivationView.java
@@ -4,6 +4,7 @@ import ch.fortylove.presentation.views.login.LoginView;
 import ch.fortylove.service.UserService;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
@@ -12,6 +13,8 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import jakarta.annotation.Nonnull;
+
+import java.util.List;
 
 @Route("activate")
 @AnonymousAllowed
@@ -32,19 +35,62 @@ public class ActivationView extends Composite implements BeforeEnterObserver {
 
     @Override
     public void beforeEnter(final BeforeEnterEvent event) {
-        final String activationCode = event.getLocation().getQueryParameters().getParameters().get("code").get(0);
-        if(userService.activate(activationCode)) {
+        final List<String> codes = event.getLocation().getQueryParameters().getParameters().get("code");
+
+        if (codes == null || codes.isEmpty()) {
+            handleMissingActivationCode();
+            return;
+        }
+
+        final String activationCode = codes.get(0);
+
+        if (checkActivationStatusOfUser(activationCode)) {
+            handleAlreadyActivated();
+        } else {
+            handleActivation(activationCode);
+        }
+    }
+
+    private void handleAlreadyActivated() {
+        layout.add(
+                new Text("Konto wurde bereits aktiviert."),
+                createLoginLink()
+        );
+    }
+
+    private void handleActivation(String activationCode) {
+        if (userService.activate(activationCode)) {
             layout.add(
                     new Text("Konto wurde erfolgreich aktiviert."),
-                    new RouterLink("Login", LoginView.class)
+                    createLoginLink()
             );
-        }
-        else {
+        } else {
             layout.add(
-                    new Text("Die Aktivierung ist fehlgeschlagen. Der Aktivierungscode ist ungültig oder bereits verwendet worden."),
+                    new Text("Die Aktivierung ist fehlgeschlagen. Der Aktivierungscode ist ungültig."),
                     new RouterLink("Registrierung erneut versuchen", RegistrationView.class),
                     new Text("Benötigen Sie Hilfe? Kontaktieren Sie uns unter fortylove.untervaz@gmail.com")
             );
         }
+    }
+
+    private void handleMissingActivationCode() {
+        String htmlContent =
+                "<div>" +
+                        "<span>Upps da ist etwas schiefgelaufen. Versuchen Sie es erneut mit demselben Aktivierungslink aus der E-Mail.</span>" +
+                        "<br>" +
+                        "<span>Benötigen Sie Hilfe? Kontaktieren Sie uns unter fortylove.untervaz@gmail.com</span>" +
+                        "</div>";
+
+        Html content = new Html(htmlContent);
+        layout.add(content);
+    }
+
+    private RouterLink createLoginLink() {
+        return new RouterLink("Login", LoginView.class);
+    }
+
+
+    private boolean checkActivationStatusOfUser(String activationCode) {
+        return userService.checkIfActive(activationCode);
     }
 }
