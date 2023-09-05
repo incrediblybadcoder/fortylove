5bc3b7c3c896d30fcdf745e4e968f12e8a3f9235
Yoohnas <jonas.cahenzli@gmail.com>
2023-09-05 20:58:36 +0200

    #210 - Bussiness Logik von der Form in die View transferiert. Form soll nur User Inputs über das UI entgegennehmen und der View (quasi Controller) über Events mitteilen (SoC)

--- src/main/java/ch/fortylove/presentation/views/registration/RegistrationForm.java
+++ src/main/java/ch/fortylove/presentation/views/registration/RegistrationForm.java
@@ -6,9 +6,9 @@ import ch.fortylove.presentation.components.ButtonFactory;
 import ch.fortylove.presentation.components.InputFieldFactory;
 import ch.fortylove.presentation.fieldvalidators.FirstNameValidator;
 import ch.fortylove.presentation.fieldvalidators.LastNameValidator;
-import ch.fortylove.service.UserService;
-import ch.fortylove.util.NotificationUtil;
-import com.vaadin.flow.component.ClickEvent;
+import ch.fortylove.presentation.views.registration.events.CancelRegistrationEvent;
+import ch.fortylove.presentation.views.registration.events.RegistrationEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H2;
@@ -19,21 +19,17 @@ import com.vaadin.flow.data.binder.ValidationResult;
 import com.vaadin.flow.data.binder.ValueContext;
 import com.vaadin.flow.data.validator.EmailValidator;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.shared.Registration;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringComponent
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @AnonymousAllowed
 public class RegistrationForm extends FormLayout {
-
-    @Nonnull private final UserService userService;
-    @Nonnull private final NotificationUtil notificationUtil;
-    @Nonnull private final PasswordEncoder passwordEncoder;
 
     @Nonnull final private Binder<User> binder;
 
@@ -49,13 +45,7 @@ public class RegistrationForm extends FormLayout {
     private String confirmPlainPasswordInput;
     private String plainPasswordInput;
 
-    public RegistrationForm(@Nonnull final UserService userService,
-                            @Nonnull final NotificationUtil notificationUtil,
-                            @Nonnull final PasswordEncoder passwordEncoder,
-                            @Nonnull final UserFactory userFactory) {
-        this.userService = userService;
-        this.notificationUtil = notificationUtil;
-        this.passwordEncoder = passwordEncoder;
+    public RegistrationForm(@Nonnull final UserFactory userFactory) {
 
         binder = new Binder<>(User.class);
         binder.bindInstanceFields(this);
@@ -92,24 +82,28 @@ public class RegistrationForm extends FormLayout {
         plainPassword.addValueChangeListener(event -> binder.validate());
         confirmPlainPassword = InputFieldFactory.createConfirmationPasswordField("Passwort bestätigen");
         register = ButtonFactory.createPrimaryButton("Registrieren", this::registerClick);
-        cancel = ButtonFactory.createNeutralButton("Abbrechen", this::gotToLoginPage);
+        cancel = ButtonFactory.createNeutralButton("Abbrechen", this::cancelClick);
     }
 
-    private void gotToLoginPage(@Nonnull final ClickEvent<Button> buttonClickEvent) {
-        buttonClickEvent.getSource().getUI().ifPresent(ui -> ui.navigate("login"));
+    private void cancelClick() {
+        fireEvent(new CancelRegistrationEvent(this));
     }
 
-    private void registerClick(@Nonnull final ClickEvent<Button> buttonClickEvent) {
+    @Nonnull
+    public Registration addCancelEventListener(@Nonnull final ComponentEventListener<CancelRegistrationEvent> listener) {
+        return addListener(CancelRegistrationEvent.class, listener);
+    }
+
+
+    private void registerClick() {
         if (binder.writeBeanIfValid(user)) {
-            if (userService.findByEmail(user.getEmail()).isPresent()) {
-                notificationUtil.errorNotification("Es gibt bereits einen Benutzer mit dieser E-Mail-Adresse.");
-            } else {
-                user.getAuthenticationDetails().setEncryptedPassword(passwordEncoder.encode(plainPassword.getValue()));
-                userService.create(user, true);
-                gotToLoginPage(buttonClickEvent);
-                notificationUtil.informationNotification("Überprüfe deine E-Mails um deine Registrierung abzuschliessen.");
-            }
+            fireEvent(new RegistrationEvent(this, user, plainPassword.getValue()));
         }
+    }
+
+    @Nonnull
+    public Registration addRegistrationEventListener(@Nonnull final ComponentEventListener<RegistrationEvent> listener) {
+        return addListener(RegistrationEvent.class, listener);
     }
 
     private void defineValidators() {
