910d2c3d64b4ff6f9f68f63765d1fcddd2311f3d
Yoohnas <jonas.cahenzli@gmail.com>
2023-09-05 23:07:17 +0200

    #202 - Impressum erster Wurf erstellt -> Review mit Marco

--- src/main/java/ch/fortylove/presentation/views/sitenotice/SiteNoticeView.java
+++ src/main/java/ch/fortylove/presentation/views/sitenotice/SiteNoticeView.java
@@ -1,20 +1,68 @@
 package ch.fortylove.presentation.views.sitenotice;
 
 import ch.fortylove.presentation.views.MainLayout;
+import com.vaadin.flow.component.html.NativeLabel;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.Nonnull;
+import jakarta.annotation.PostConstruct;
 import jakarta.annotation.security.PermitAll;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Route(value = SiteNoticeView.ROUTE, layout = MainLayout.class)
 @PageTitle(SiteNoticeView.PAGE_TITLE)
 @PermitAll
+@Component
 public class SiteNoticeView extends VerticalLayout {
 
     @Nonnull public static final String ROUTE = "sitenotice";
     @Nonnull public static final String PAGE_TITLE = "Impressum";
 
-    public SiteNoticeView(){
+    @Value("${siteNotice.title}")
+    private String title;
+
+    @Value("${siteNotice.name}")
+    private String name;
+
+    @Value("${siteNotice.street}")
+    private String street;
+
+    @Value("${siteNotice.plz}")
+    private String plz;
+
+    @Value("${siteNotice.city}")
+    private String city;
+
+    @Value("${siteNotice.country}")
+    private String country;
+
+    @Value("${siteNotice.email}")
+    private String email;
+
+    @Value("${siteNotice.phone}")
+    private String phone;
+
+    @Value("${siteNotice.website}")
+    private String website;
+
+    @Value("${siteNotice.additionalInfo}")
+    private String additionalInfo;
+
+    public SiteNoticeView() {
     }
+
+    @PostConstruct
+    public void init() {
+        add(new NativeLabel(name));
+        add(new NativeLabel(street));
+        add(new NativeLabel(plz + " " + city));
+        add(new NativeLabel(country));
+        add(new NativeLabel("E-Mail: " + email));
+        add(new NativeLabel("Telefon: " + phone));
+        add(new NativeLabel("Website: " + website));
+        add(new NativeLabel(additionalInfo));
+    }
+
 }
