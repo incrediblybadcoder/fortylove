cba4c797bc6e3249675877f2370cfca2c8b5e0ed
Yoohnas <jonas.cahenzli@gmail.com>
2023-09-08 18:18:45 +0200

    #221 - Link auf Homepage des TC Untervaz hinzugefügt

--- src/main/java/ch/fortylove/presentation/components/managementform/SupportComponent.java
+++ src/main/java/ch/fortylove/presentation/components/managementform/SupportComponent.java
@@ -19,32 +19,29 @@ public class SupportComponent extends VerticalLayout {
     public SupportComponent() {
 
         Paragraph description = new Paragraph("Haben Sie Fragen oder Probleme? Wir sind hier, um Ihnen zu helfen!");
-
-
         Div descriptionContainer = new Div(description);
         descriptionContainer.setWidthFull();
         descriptionContainer.getStyle().set("text-align", "center");
 
 
         H3 clubContact = new H3("Clubkontakt");
-        Anchor homepageLink = new Anchor("https://www.tcuntervaz.ch/kontakt-1/", "TC Untervaz");
+        Anchor homepageLink = new Anchor("https://www.tcuntervaz.ch/kontakt-1/", "TC Untervaz - Kontakte");
         homepageLink.setTarget("_blank");
+        Div clubContactContainer = new Div(clubContact);
+        clubContactContainer.setWidthFull();
+        clubContactContainer.getStyle().set("text-align", "center");
 
         H3 systemContact = new H3("Kontakt für technische Probleme");
-
         Div systemContactContainer = new Div(systemContact);
         systemContactContainer.setWidthFull();
         systemContactContainer.getStyle().set("text-align", "center");
 
         Paragraph supportEmail = new Paragraph("fortylove.untervaz@gmail.com");
 
-
         add(descriptionContainer, clubContact, homepageLink, systemContactContainer, supportEmail);
-
 
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
         setSpacing(true);
-
     }
 }
