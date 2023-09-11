29470f075751459378ff0add31001bb93ca1097c
incrediblybadcoder <m.solombrino@hotmail.ch>
2023-09-11 12:22:05 +0200

    #238 BookingView Artikel und Plätze besser aufteilen - refactored booking view without BookingComponent [skip ci]

---
+++ src/main/java/ch/fortylove/presentation/views/booking/bookinggrid/events/BookedCellClickEvent.java
@@ -0,0 +1,39 @@
+package ch.fortylove.presentation.views.booking.bookinggrid.events;
+
+import ch.fortylove.persistence.entity.Booking;
+import ch.fortylove.persistence.entity.Court;
+import ch.fortylove.persistence.entity.Timeslot;
+import ch.fortylove.presentation.views.booking.bookinggrid.BookingGrid;
+import jakarta.annotation.Nonnull;
+
+public class BookedCellClickEvent extends BookingGridEvent {
+
+    @Nonnull private final Court court;
+    @Nonnull private final Timeslot timeslot;
+    @Nonnull private final Booking booking;
+
+    public BookedCellClickEvent(@Nonnull final BookingGrid source,
+                                @Nonnull final Court court,
+                                @Nonnull final Timeslot timeslot,
+                                @Nonnull final Booking booking) {
+        super(source);
+        this.court = court;
+        this.timeslot = timeslot;
+        this.booking = booking;
+    }
+
+    @Nonnull
+    public Court getCourt() {
+        return court;
+    }
+
+    @Nonnull
+    public Timeslot getTimeSlot() {
+        return timeslot;
+    }
+
+    @Nonnull
+    public Booking getBooking() {
+        return booking;
+    }
+}
---
+++ src/main/java/ch/fortylove/presentation/views/booking/bookinggrid/events/FreeCellClickEvent.java
@@ -0,0 +1,30 @@
+package ch.fortylove.presentation.views.booking.bookinggrid.events;
+
+import ch.fortylove.persistence.entity.Court;
+import ch.fortylove.persistence.entity.Timeslot;
+import ch.fortylove.presentation.views.booking.bookinggrid.BookingGrid;
+import jakarta.annotation.Nonnull;
+
+public class FreeCellClickEvent extends BookingGridEvent {
+
+    @Nonnull private final Court court;
+    @Nonnull private final Timeslot timeslot;
+
+    public FreeCellClickEvent(@Nonnull final BookingGrid source,
+                              @Nonnull final Court court,
+                              @Nonnull final Timeslot timeslot) {
+        super(source);
+        this.court = court;
+        this.timeslot = timeslot;
+    }
+
+    @Nonnull
+    public Court getCourt() {
+        return court;
+    }
+
+    @Nonnull
+    public Timeslot getTimeSlot() {
+        return timeslot;
+    }
+}
